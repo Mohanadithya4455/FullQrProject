c021e380c1135205590bc4fa66131fb4daa0e8e5
Mohanadithya4455 <147871910+Mohanadithya4455@users.noreply.github.com>
2024-04-27 12:18:04 +0530

    Add files via upload

---
+++ QrCode/BarCode/src/main/java/com/example/demo/DTO/Sales.java
@@ -0,0 +1,43 @@
+package com.example.demo.DTO;
+
+public class Sales {
+	
+	private Integer productId;
+	private String productName;
+	private String productWeight;
+	private String weight;
+	private String productPrice;
+
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductWeight() {
+		return productWeight;
+	}
+	public void setProductWeight(String productWeight) {
+		this.productWeight = productWeight;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+
+
+}
